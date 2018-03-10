c7a4c336d8d2729a2c6cdb6896e82138148a545a
code4GG <cavallaro.av@gmail.com>
2018-03-10 10:22:34 -0500

    heroku db

--- config/connection.js
+++ config/connection.js
@@ -8,14 +8,18 @@ const connection = mysql.createConnection({
   database: "burgers_db"
 });
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+if (process.env.JAWSDB_URL){
+    connection.connect(process.env.JAWSDB_URL)
+} else {
+    connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+}
+
 
 // Export connection for our ORM to use.
 module.exports = connection;
