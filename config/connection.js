5f02279cd576d31524d19acc3052734c46d99280
code4GG <cavallaro.av@gmail.com>
2018-03-10 11:55:01 -0500

    connection

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,23 @@
 const mysql = require("mysql");
-
-const connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+let connection;
+// const connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "root",
+//   database: "burgers_db"
+// });
 
 if (process.env.JAWSDB_URL){
     connection.connect(process.env.JAWSDB_URL)
 } else {
+    connection = mysql.createConnection({
+      port: 3306,
+      host: "localhost",
+      user: "root",
+      password: "root",
+      database: "burgers_db"
+    });
     connection.connect(function(err) {
     if (err) {
       console.error("error connecting: " + err.stack);
