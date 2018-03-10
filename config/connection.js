2d5944ae10b5dcb51afb557519cd6be91bfb0a3a
code4GG <cavallaro.av@gmail.com>
2018-03-10 12:01:30 -0500

    connection2

--- config/connection.js
+++ config/connection.js
@@ -18,15 +18,14 @@ if (process.env.JAWSDB_URL){
       password: "root",
       database: "burgers_db"
     });
-    connection.connect(function(err) {
+}
+connection.connect(function(err) {
     if (err) {
       console.error("error connecting: " + err.stack);
       return;
     }
     console.log("connected as id " + connection.threadId);
   });
-}
-
 
 // Export connection for our ORM to use.
 module.exports = connection;
